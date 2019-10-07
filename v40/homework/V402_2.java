8b36cbddfacf6df27f4bed1facd5b6c5ffbd0949
pofor <pontus.forslermott@elev.ga.ntig.se>
2019-10-07 10:38:30 +0200

    Update V402_2.java

--- v40/homework/V402_2.java
+++ v40/homework/V402_2.java
@@ -1,5 +1,24 @@
+
 package homework;
-
+import java.util.Scanner;
 public class V402_2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int fizz = input.nextInt();
+		int buzz = input.nextInt();
+		int n = input.nextInt();
+		
+		for( int i = 1; i<= n; i++)	{
+			if(1 % fizz == 0 && 1 % buzz == 0) {
+				System.out.print ("Fizzbuzz");}
+			
+			else if(i % fizz == 0) {System.out.println("fizz");
+			}
+			else if(i % buzz == 0) {System.out.println("buzz");}
+			else {System.out.println(i);
+			}
+			}
+		}
+	}
 
-}
