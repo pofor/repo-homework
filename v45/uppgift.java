2fcd13ab51375ba8213b6d8e3f324d656c4b9ca3
pofor <pontus.forslermott@elev.ga.ntig.se>
2019-11-11 10:40:42 +0100

    Här kommer andra uppgiften kommer med java dokumenteringar när jag är klar med allt.

--- v45/uppgift.java
+++ v45/uppgift.java
@@ -2,10 +2,15 @@ public class uppgift {
 	
 public static void main(String[] args) {
 	System.out.println(farenheitToCelcius (50));
+	System.out.println(KelvinToCelcius(0));
 }
 public static double farenheitToCelcius (double farenheit) {
 	double celcius = ((farenheit - 32)*5)/9;
 	return celcius; 
 }
-
+public static double KelvinToCelcius (double kelvin) {
+	double celcius = (kelvin-273.15);
+	return celcius;
+	
+}
 }
